64a149da552904dc27611dfe90c4358405008e47
Ushahemba Shir <laleoict@gmail.com>
2020-04-22 17:23:56 +0100

    php connection added but failed to connect

--- process.php
+++ process.php
@@ -1,13 +1,26 @@
 <?php
 
-echo 'Processing';
+// Connect to a database
+$conn - mysqli_connect('localhost', 'root', '', 'ajaxtest');
+echo 'Processing...';
 
-// Check fir a GET variable
+// Check for POST variable
 if(isset($_POST['name'])){
-    echo 'GET: Your name is '.$_POST['name'];
+    $name = mysqli_real_string($conn, $_POST['name']);
+    // echo 'POST: Your name is '.$_POST['name'];
+
+    $query = "INSERT INTO users(name) VALUES('$name')";
+
+    if(mysqli_query($conn, $query)){
+        echo 'User added succesfully...';
+    } else {
+        echo 'ERROR: '. mysqli_error($conn);
+    }
 }
 
-// Check fir a GET variable
+// Check for GET variable
 if(isset($_GET['name'])){
     echo 'GET: Your name is '.$_GET['name'];
 }
+
+?>
